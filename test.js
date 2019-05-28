fd23ab1c8fd945dcc42b3e68d324a8f1ad51d245
Joanna Skrzypińska <jm.skrzypinska@gmail.com>
2019-05-28 17:20:14 +0100

    this keyword

--- test.js
+++ test.js
@@ -1,27 +1,16 @@
-var myArray = new Array();
-
-myArray[0] = 8;
-myArray[1] = "hello";
-
-var myCar = new Object();
-myCar.maxSpeed = 50; //properities of my car 
-myCar.driver = "Asia";
-myCar.drive = function () {console.log("now im driving");};
-
-console.log(myCar.driver);
-
-
-myCar.drive();
-
-var myArray2 = [true,15, "hello"];
-
 var myCar2 = { 
     maxSpeed: 40, 
     driver: "Asia", 
     drive: function (speed, time) {
             console.log(speed*time);
-    } 
+    }, 
+    logDriver: function() {
+        console.log("driver name is " + this.driver);
+    }
 };
+
+
+myCar2.logDriver();
 
 console.log(myCar2.maxSpeed);
 myCar2.drive(50,3);
