c556eaff0807a1114398c32c776d661d4381c7a2
Joanna Skrzypińska <jm.skrzypinska@gmail.com>
2019-05-28 14:36:46 +0100

    if/else statment

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-alert("Hello world!");
+var youLike=false;
+var myNum = 9;
+
+if(myNum == 10) {
+    document.write("My numb is equel to 10");
+} else {
+    document.write("My number is not equel to 10")
+}
