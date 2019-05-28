d10aea2e6631de4fbc550ece5c6061885d31df74
Joanna Skrzypińska <jm.skrzypinska@gmail.com>
2019-05-28 17:35:00 +0100

    constructor functions - object

--- test.js
+++ test.js
@@ -1,16 +1,20 @@
-var myCar2 = { 
-    maxSpeed: 40, 
-    driver: "Asia", 
-    drive: function (speed, time) {
-            console.log(speed*time);
-    }, 
-    logDriver: function() {
+
+var Car = function (maxSpeed, driver) {
+
+    this.maxSpeed = maxSpeed; // property
+    this.driver = driver; // property
+    this.drive = function (speed, time) {
+        console.log(speed*time); // method
+    },
+    this.logDriver = function() { //method
         console.log("driver name is " + this.driver);
     }
-};
+}
 
+var myCar = new Car (50, "Asia");
+var myCar2 = new Car (3, "Gosia");
+var myCar3 = new Car (15, "Hela");
+var myCar4 = new Car (2, "Basia");
 
-myCar2.logDriver();
-
-console.log(myCar2.maxSpeed);
-myCar2.drive(50,3);
+myCar.drive(30,5);
+myCar3.logDriver();
