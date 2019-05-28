b3f5da0ddd5e03107776e09fa63de9dc322e9667
Joanna Skrzypińska <jm.skrzypinska@gmail.com>
2019-05-28 17:28:27 +0100

    strings

--- test.js
+++ test.js
@@ -1,8 +1,18 @@
-var a = "apple";
-var b = 5;
+// var string1 = "abc";
+// var string2 = "ABC";
 
-if (isNaN(a)) {
-    console.log("is not a number");
-} else {
-    console.log("meaning of life is " + (a*b));
-}
+// console.log(string1.toLowerCase() === string2.toLowerCase()); 
+
+
+// console.log(myString.length);
+// console.log(myString.toUpperCase());
+// console.log(myString.indexOf("string"));
+
+// if (myString.indexOf("ninja") === -1 ) {
+//     console.log("word is not in the string");
+// } else {
+//     console.log("word ninja starts at position " + myString.indexOf("ninja"));
+// }
+  var string1 ="abc";
+  var string2 ="bcd";
+  console.log(string1 < string2);
