197d6af92472b6187829b90adc48610780d32f09
Joanna Skrzypińska <jm.skrzypinska@gmail.com>
2019-05-28 22:36:35 +0100

    simple form validation

--- test.js
+++ test.js
@@ -1,29 +1,14 @@
-// var myMessage = document.getElementById("message");
+var myForm = document.forms.myForm;
+var messege = document.getElementById("message");
 
-// function showMessage () {
-//     myMessage.className ="show";
-// }
+myForm.onsubmit = function () {
 
-// setTimeout(showMessage, 3000);
-
-var colourChanger = document.getElementById("colour-changer");
-var colours =["red", "blue", "green"];
-var counter = 0;
-
-function changeColour() {
-
-    if (counter >= colours.length) {
-        counter = 0;
+    if(myForm.name.value == "") {
+        messege.innerHTML = "Please enter a name";
+        return false;
+    } else {
+        message.innerHTML = "";
+        return true;
     }
 
-    colourChanger.style.background = colours[counter];
-    counter++;
-}
-
-var myTimer = setInterval(changeColour, 2000);
-
-colourChanger.onclick = function () {
-
-    clearInterval(myTimer);
-    colourChanger.innerHTML = "Timer stopped";
-}
+};
